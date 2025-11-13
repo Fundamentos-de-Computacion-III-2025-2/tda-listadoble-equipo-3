8ec4c9bd40447d1aec527180f4e783ff4770ee30
Joaquin <joaquindaviladavila@gmail.com>
2025-11-13 11:07:57 -0700

    Se arregla la rama para el nuevo merge

--- src/ListaDoble.java
+++ src/ListaDoble.java
@@ -2,28 +2,40 @@ public class ListaDoble {
     protected NodoDoble inicio, fin; //Apuntadores para saber donde esta el inicio y el fin de la lista doble
 
     //Constructor para crear la lista doble vacía
-    public ListaDoble(){
+    public ListaDoble() {
         inicio = null;
-        fin =null;
+        fin = null;
     }
 
     //Metodo para saber si la lista doble está vacía
-    public boolean listaVacia(){
-        if(inicio==null){
+    public boolean listaVacia() {
+        if (inicio == null) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    //Metodo para agregar un NodoDoble al Inicio de la Lista Doble JOAQUIN
-    public void insertarInicio(int dato){
+    //Metodo para agregar un NodoDoble al Inicio de la Lista Doble.
+    public void insertarInicio(int dato) {
+        NodoDoble nuevoNodo = new NodoDoble(dato);
 
+        //lista vacía
+        if (listaVacia()) {
+            inicio = nuevoNodo;
+            fin = nuevoNodo;
+        }
 
+        //lista no vacía
+        else {
+            nuevoNodo.siguiente = inicio;
+            inicio.anterior = nuevoNodo;
+            inicio = nuevoNodo;
+        }
     }
 
     //Metodo para insertar al Final de la lista doble,ERICK
-    public void insertarFinal(int dato){
+    public void insertarFinal(int dato) {
 
     }
 
@@ -34,48 +46,59 @@ public class ListaDoble {
     se inserta al final */
 
     //JAVIER
-    public void insertarEnOrden(int dato){
+    public void insertarEnOrden(int dato) {
 
     }
 
 
     //Eliminar al inicio VICTORIA
-    public int eliminarInicio(){
+    public int eliminarInicio() {
 
         return -1;
     }
 
     //Eliminar al final JAVIER
-    public int eliminarFinal(){
+    public int eliminarFinal() {
 
         return -1;
     }
 
     //Eliminar un elemento ERICK
-    public int eliminarElemento(int elemento){
+    public int eliminarElemento(int elemento) {
 
         return elemento;
     }
 
-    //Metodo para buscar un elemento JOAQUIN
-    public boolean buscarElemento(int elemento){
+    //Metodo para buscar un elemento
+    public boolean buscarElemento(int elemento) {
+        if(listaVacia()){
+            return false;
+        }
+
+        NodoDoble actual = inicio;
+
+        while(actual != null){
+            if(actual.dato == elemento){
+                return true;
+            }
+            actual = actual.siguiente;
+        }
 
         return false;
-
     }
 
     //Imprimir los datos de la lista doble de inicio a fin
-    public void mostrarInicioFin(){
-        NodoDoble actual=inicio;
+    public void mostrarInicioFin() {
+        NodoDoble actual = inicio;
         System.out.println();
-        while(actual!=null){
-            System.out.print(actual.dato+" --> ");
+        while (actual != null) {
+            System.out.print(actual.dato + " --> ");
             actual = actual.siguiente;
         }
     }
 
     //Imprimir los datos de la lista doble de fin a inicio VICTORIA
-    public void mostrarFinInicio(){
+    public void mostrarFinInicio() {
 
     }
 
